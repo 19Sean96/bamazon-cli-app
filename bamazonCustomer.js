f9c047033101018865ec50ee262624648be23b22
Sean A <1996.sean.alexander@gmail.com>
2019-08-11 13:33:20 -0700

    added Manager view (view db, view low inventory db, update Inventory

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -3,21 +3,21 @@ const inquirer = require("inquirer");
 
 const connection = mysql.createConnection({
     host: "localhost",
-  
+
     // Your port; if not 3306
     port: 3306,
-  
+
     // Your username
     user: "root",
-  
+
     // Your password
     password: "root",
     database: "bamazon"
-  });
+});
 
-  let productArr;
+let productArr;
 
-  connection.connect((err) => {
+connection.connect(err => {
     if (err) throw err;
     console.log("connected as id " + connection.threadId + "\n");
     // createProduct();
@@ -25,113 +25,258 @@ const connection = mysql.createConnection({
     readProducts();
 
     init();
-  });
+});
 
-  function init() {
+function init() {
+    inquirer
+        .prompt([
+            {
+                name: "role",
+                type: "list",
+                choices: ["Customer", "Manager", "Supervisor"],
+                message: "Welcome! WHat is your role?"
+            }
+        ])
+        .then(ans => {
+            console.log(ans);
+            let role = ans.role;
 
-    inquirer.prompt([
-        {
-            name: "role",
-            type: "list",
-            choices: [
-                "Customer",
-                "Manager",
-                "Supervisor"
-            ],
-            message: "Welcome! WHat is your role?"
-        }
-    ]).then((ans) => {
-        console.log(ans);
-        let role = ans.role;
-
-        if (role === 'Customer') {
-
-            console.log(`\n
+            if (role === "Customer") {
+                console.log(`\n
                          ===============================\n
                          Thank you for choosing BAMAZON!\n
                          =============================== `);
-            console.log(`\n
+                console.log(`\n
                          ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n
                          Take a look at our merchandise.\n
                          =============================== 
                          |PRODUCT ID|  |PRODUCT NAME| |PRICE|`);
-            let idList = [];
-            productArr.forEach(product => {
+                let idList = [];
+                productArr.forEach(product => {
+                    console.log(`
+                            |${product.id}|--|${product.product_name}|--|${
+                        product.price
+                    }|`);
+                    idList.push(product.id);
+                });
+                inquirer
+                    .prompt([
+                        {
+                            name: "shoppingCart",
+                            type: "checkbox",
+                            choices: idList,
+                            message: "What Would you like to buy? (See above)"
+                        }
+                    ])
+                    .then(ans => {
+                        let shoppingCart = [];
+                        ans.shoppingCart.forEach(productID => {
+                            let j = 0;
+                            for (let i = 1; i <= productArr.length; i++) {
+                                if (i === productID) {
+                                    shoppingCart.push(productArr[i - 1]);
+                                }
+                            }
+                        });
+                        shoppingCart.forEach(cartItem => {
+                            cartItem.cost = 0;
+                        });
+                        calcPurchaseQuantity(
+                            shoppingCart,
+                            shoppingCart.length,
+                            0
+                        );
+                    });
+            } else if (role === "Manager") {
+                console.log(`\n
+              ===============================\n
+              Welcome Back BAMAZON Manager!\n
+              =============================== `);
+
+                managerPortal();
+            }
+        });
+}
+
+function managerPortal() {
+    inquirer
+        .prompt([
+            {
+                name: "option",
+                type: "list",
+                choices: [
+                    "View Products for Sale",
+                    "View Low Inventory",
+                    "Restock Inventory",
+                    "Add New Product"
+                ],
+                message: "What Would You Like To Do?"
+            }
+        ])
+        .then(ans => {
+            console.log(ans.option);
+
+            if (ans.option === "View Products for Sale") {
+                console.log(`\n
+                  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n
+                  CURRENT INVENTORY:\n
+                  =============================== 
+                  |PRODUCT ID|  |PRODUCT NAME| |PRICE| |INVENTORY|`);
+                productArr.forEach(product => {
+                    console.log(`
+                      |${product.id}|--|${product.product_name}|--|${product.price}|--|${product.stock_quantity}|`);
+                });
+                checkForExtraQuery();
+            } else if (ans.option === "View Low Inventory") {
+                readLowInventory();
+            } else if (ans.option === "Restock Inventory") {
+                restockInventory();
+            } else {
+            }
+        });
+}
+
+function checkForExtraQuery() {
+  inquirer.prompt([
+    {
+      type: "list",
+      name: "again",
+      choices: ["Yes","No"],
+      message: "Do You Have Another Task To Complete?"
+    }
+  ]).then(ans => {
+    console.log(ans.again);
+
+    if (ans.again === "Yes") {
+      managerPortal();
+    } else {
+      console.log("HAVE A GREAT DAY!");
+      connection.end();
+    }
+  });
+}
+
+function calcPurchaseQuantity(item, countdown, i) {
+    inquirer
+        .prompt([
+            {
+                type: "number",
+                name: "quantity",
+                message: `How Many Of The ${
+                    item[i].product_name
+                } Would You Like TO Purchase? (Only ${
+                    item[i].stock_quantity
+                } Remain!)`
+            }
+        ])
+        .then(ans => {
+            console.log(ans.quantity);
+            if (ans.quantity <= item[i].stock_quantity) {
+                item[i].stock_quantity -= ans.quantity;
+                item[i].cost += item[i].price * ans.quantity;
+                console.log(item);
+                i++;
+                countdown--;
+            } else {
                 console.log(`
-                            |${product.id}|--|${product.product_name}|--|${product.price}|`);
-                idList.push(product.id);
-            });
-            inquirer.prompt([
-              {
-                name: "shoppingCart",
-                type: "checkbox",
-                choices: idList,
-                message: "What Would you like to buy? (See above)"
-              }
-            ]).then(ans => {
-              let shoppingCart =[];
-              ans.shoppingCart.forEach(productID => {
-                let j = 0;
-                for (let i = 1; i <= productArr.length; i++) {
-                  if (i === productID) {
-                    shoppingCart.push(productArr[i - 1]);
-                  }
-                }
-              });
-              shoppingCart.forEach(cartItem => {
-                cartItem.cost = 0;
-              })
-              calcPurchaseQuantity(shoppingCart, shoppingCart.length, 0);
-            });
-            
-        }
-    });
-  }
-
-  function calcPurchaseQuantity(item, countdown, i) {
-    inquirer.prompt([
-      {
-        type: "number",
-        name: "quantity",
-        message: `How Many Of The ${item[i].product_name} Would You Like TO Purchase? (Only ${item[i].stock_quantity} Remain!)`
-      }
-    ]).then(ans => {
-
-      console.log(ans.quantity);
-      if (ans.quantity <= item[i].stock_quantity) {
-        item[i].stock_quantity -= ans.quantity;
-        item[i].cost += (item[i].price * ans.quantity);
-        console.log(item);
-        i++;
-        countdown--;
-      } else {
-        console.log(`
                     _________________________________________________________________
                    | Uh oh! We don't have enough of that! Please order a lower amount|
                    |_________________________________________________________________|`);
-      }
-      if(countdown > 0) {
-        calcPurchaseQuantity(item, countdown, i);
-      } else updateInventory(item);
-    });
-  }
+            }
+            if (countdown > 0) {
+                calcPurchaseQuantity(item, countdown, i);
+            } else updateInventory(item);
+        });
+}
 
-  function updateInventory(newInventory) {
+function updateInventory(newInventory) {
     let totalcost = 0;
     newInventory.forEach(item => {
-      totalcost += item.cost;
-      connection.query(`UPDATE products SET stock_quantity = ${item.stock_quantity} WHERE id = ${item.id}`, function(err, res) {
-        if (err) throw err;
-      });
+        totalcost += item.cost;
+        connection.query(
+            `UPDATE products SET stock_quantity = ${
+                item.stock_quantity
+            } WHERE id = ${item.id}`,
+            function(err, res) {
+                if (err) throw err;
+            }
+        );
     });
     console.log(`
     ===========================================================================
-    YOUR TOTAL COST IS $${totalcost.toFixed(2)}. THANK YOU FOR CHOOSING BAMAZON!\n`);connection.end();
-  }
-  
-  function readProducts() {
+    YOUR TOTAL COST IS $${totalcost.toFixed(
+        2
+    )}. THANK YOU FOR CHOOSING BAMAZON!\n`);
+    connection.end();
+}
+
+function readProducts() {
     connection.query("SELECT * FROM products", function(err, res) {
-      if (err) throw err;
-      productArr = res;
+        if (err) throw err;
+        productArr = res;
     });
+}
+
+function readLowInventory() {
+  connection.query("SELECT * FROM products WHERE stock_quantity < 5", function(err, res) {
+    if (err) throw err;
+    
+    console.log(res);
+    console.log(`\n
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n
+    LOW INVENTORY ITEMS:\n
+    =============================== 
+    |PRODUCT ID|  |PRODUCT NAME| |PRICE| |INVENTORY|`);
+    res.forEach(product => {
+      console.log(`
+        |${product.id}|--|${product.product_name}|--|${product.price}|--|${product.stock_quantity}|`);
+    });
+    checkForExtraQuery();
+  });
+}
+
+function readProdByID(res) {
+  const id = res.productID.charAt(0);
+  const product = productArr.filter(product => {
+    if (product.id === parseInt(id)) {
+      return product;
+    }
+  });
+  product[0].stock_quantity += res.quantity
+  console.log(product[0]);
+  connection.query(
+    `UPDATE products SET stock_quantity = ${
+        product[0].stock_quantity
+    } WHERE id = ${id}`,
+    function(err, res) {
+        if (err) throw err;
+        readProducts();
+        console.log(`
+          *****INVENTORY HAS BEEN UPDATED*****
+        `);
+        checkForExtraQuery();
+      }
+);
+}
+
+function restockInventory() {
+  let totalProductCount = productArr.length;
+  let idList = [];
+  for (let i = 1; i <= totalProductCount; i++) {
+    idList.push(i + "-" + productArr[i-1].product_name);
   }
+  console.log(idList);
+  inquirer.prompt([
+    {
+      type: "list",
+      name: "productID",
+      choices: idList,
+      message: "Which Product Would You Like To Restock?"
+    },
+    {
+      type: "number",
+      name: "quantity",
+      message: "How Many Products Would You Like to Restock?"
+    }
+  ]).then(ans => readProdByID(ans));
+}
